c16d76e8a63c8f67001e190cea3abf309d5f2a24
Curtis Grotzke <cugrotzke@gmail.com>
2025-01-21 16:03:47 -0800

    index.ts

--- components/index.ts
+++ components/index.ts
@@ -2,12 +2,10 @@ import Hero from "./Hero";
 import About from "./About";
 import Projects from "./Projects";
 import Contact from "./Contact";
-import Sticky from "./Sticky";
 
 export default {
     Hero,
     Projects,
     About,
     Contact,
-    Sticky
 }
